60d76de54d44e1179986b08f15ca16623b715a08
Pratulya Pratap <pratulyapratap@gmail.com>
2022-10-30 22:39:34 +0530

    Reverse an array 1

--- src/Arrays/ReverseAnArray.java
+++ src/Arrays/ReverseAnArray.java
@@ -2,9 +2,27 @@ package Arrays;
 
 public class ReverseAnArray {
 
+	public static void reverse1(int arr[])
+	{
+		int left = 0; int right = arr.length-1;
+		while(left <= right)
+		{
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left++;
+			right--;
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int arr[] = {1,2,3,4,5};
+		reverse1(arr);
+		for(int i: arr)
+		{
+			System.out.print(i+" ");
+		}
 	}
 
 }
